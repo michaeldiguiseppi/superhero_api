54de3abbfa6d95e7ab8630c56e61a6f927181e9b
Michael DiGuiseppi <michael.diguiseppi@echostar.com>
2017-03-03 12:11:07 -0700

    Updated/refactored routes and cleaned up code.

--- routes/api.js
+++ routes/api.js
@@ -1,58 +1,57 @@
+/*===========* Requires *===============*/
+
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Superhero = mongoose.model('superheroes');
 
-/* GET api page. */
-router.get('/superheroes', function(req, res, next) {
+/*===========* Routing Functions *===============*/
+
+router.get('/superheroes', getAllSuperheroes);
+router.get('/superhero/:id', getOneSuperhero);
+router.post('/superheroes', addSuperhero);
+router.put('/superhero/:id', updateSuperhero);
+router.delete('/superhero/:id', deleteSuperhero);
+
+/*===========* Helper Functions *===============*/
+
+function getAllSuperheroes(req, res, next) {
   Superhero.find(function(err, superheroes) {
-    console.log(superheroes);
-    res.render('api', { title: "Superhero API", superheroes: superheroes });
-  });
-});
+     res.render('api', { title: "Superhero API", superheroes: superheroes });
+   });
+}
 
-
-router.post('/superheroes', function(req, res, next) {
-  new Superhero({name: req.body.name, superpower: req.body.power})
-    .save(function(err, superhero) {
-      console.log(superhero);
-      res.redirect('/api/superheroes');
-    });
-});
-
-router.get('/superhero/:id', function(req, res, next) {
+function getOneSuperhero(req, res, next) {
   var query = { "_id": req.params.id };
 
   Superhero.findOne(query, function(err, superhero) {
-    console.log(superhero);
     res.render('superhero', { title: "Superhero API - " + superhero.name, superhero: superhero });
   });
-});
+}
 
+function addSuperhero(req, res, next) {
+  new Superhero({name: req.body.name, superpower: req.body.power})
+    .save(function(err, superhero) {
+      res.redirect('/api/superheroes');
+    });
+}
 
-router.put('/superhero/:id', function(req, res, next) {
+function updateSuperhero(req, res, next) {
   var query = { "_id": req.params.id };
   var update = { name: req.body.name, superpower: req.body.power };
   var options = { new: true };
 
   Superhero.findOneAndUpdate(query, update, options, function(err, superhero) {
-    console.log(superhero);
     res.render('superhero', { title: 'Superhero API - ' + superhero.name, superhero: superhero });
   });
+}
 
-});
-
-
-router.delete('/superhero/:id', function(req, res, next) {
+function deleteSuperhero(req, res, next) {
   var query = { "_id": req.params.id };
   Superhero.findOneAndRemove(query, function(err, superhero) {
-    console.log(superhero);
     res.redirect('/api/superheroes');
   });
-});
-
-
-
+}
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
+/*===========* Requires *===============*/
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/*===========* Routing Functions *===============*/
+
+router.get('/', getHomeRoute);
+
+/*===========* Helper Functions *===============*/
+
+function getHomeRoute(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+}
 
 module.exports = router;
